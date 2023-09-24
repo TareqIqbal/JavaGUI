b3a15a884e50ba3ed3d0913e6f29795261cbf4fe
TareqIqbal <99907300+TareqIqbal@users.noreply.github.com>
2023-09-24 12:44:16 +0600

    Create Message.java

---
+++ Message.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Message
+  {
+    public static void main(String args[])
+    {
+      JOptionPane.showMessageDialog(null,"HELLO WORLD!!!","Tareq's First Message",1);
+    }
+  }
